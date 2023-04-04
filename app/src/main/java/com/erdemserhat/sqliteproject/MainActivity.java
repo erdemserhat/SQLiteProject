9057bcefddf07ca57c3e3092d72c018233dd9a81
Serhat Erdem <me.serhaterdem@gmail.com>
2023-04-04 12:04:46 +0300

    filtering and auto incrementing id

--- app/src/main/java/com/erdemserhat/sqliteproject/MainActivity.java
+++ app/src/main/java/com/erdemserhat/sqliteproject/MainActivity.java
@@ -20,19 +20,24 @@ public class MainActivity extends AppCompatActivity {
             //Operations which will be tried.
             //Creating Main Database
             SQLiteDatabase database = this.openOrCreateDatabase("Students",MODE_PRIVATE, null);
-            //Creating a sheet/table and columns.
-            database.execSQL("CREATE TABLE IF NOT EXISTS students (name VARCHAR, age INT)");
+            //Creating a sheet/table and columns, auto incrementing id with  "INTEGER PRIMARY KEY"
+            database.execSQL("CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY,name VARCHAR, age INT)");
             //Inserting data
-            database.execSQL("INSERT INTO students (name, age) VALUES ('Serhat',22)");
-            //Reading Data
-            Cursor cursor = database.rawQuery("SELECT * FROM students",null);
+            //database.execSQL("INSERT INTO students (name, age) VALUES ('Serhat',22)");
+            //database.execSQL("INSERT INTO students (name, age) VALUES ('Ali',31)");
+            //database.execSQL("INSERT INTO students (name, age) VALUES ('Volkan',30)");
+
+            //Reading Data, filtering data by using where keyword
+            Cursor cursor = database.rawQuery("SELECT * FROM students where id=3",null);
 
             int nameIx = cursor.getColumnIndex("name");
             int ageIx = cursor.getColumnIndex("age");
+            int idIx = cursor.getColumnIndex("id");
 
             while(cursor.moveToNext()){
                 System.out.println("Name : " + cursor.getString(nameIx));
                 System.out.println("Age : " + cursor.getInt(ageIx));
+                System.out.println("ID : " + cursor.getInt(idIx));
 
             }
 
