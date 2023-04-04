68ff44d54fe69c6e5824760840ef9a068b95d885
Serhat Erdem <me.serhaterdem@gmail.com>
2023-04-04 08:52:21 +0300

    Inserting and Reading Data

--- app/src/main/java/com/erdemserhat/sqliteproject/MainActivity.java
+++ app/src/main/java/com/erdemserhat/sqliteproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.erdemserhat.sqliteproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
@@ -18,9 +19,24 @@ public class MainActivity extends AppCompatActivity {
         try {
             //Operations which will be tried.
             //Creating Main Database
-            SQLiteDatabase database = this.openOrCreateDatabase("MajorDataBaseName",MODE_PRIVATE, null);
+            SQLiteDatabase database = this.openOrCreateDatabase("Students",MODE_PRIVATE, null);
             //Creating a sheet/table and columns.
-            database.execSQL("CREATE TABLE IF NOT EXISTS tableName (column1 VARCHAR, colum2 INT)");
+            database.execSQL("CREATE TABLE IF NOT EXISTS students (name VARCHAR, age INT)");
+            //Inserting data
+            database.execSQL("INSERT INTO students (name, age) VALUES ('Serhat',22)");
+            //Reading Data
+            Cursor cursor = database.rawQuery("SELECT * FROM students",null);
+
+            int nameIx = cursor.getColumnIndex("name");
+            int ageIx = cursor.getColumnIndex("age");
+
+            while(cursor.moveToNext()){
+                System.out.println("Name : " + cursor.getString(nameIx));
+                System.out.println("Age : " + cursor.getInt(ageIx));
+
+            }
+
+            cursor.close();
 
 
         } catch (Exception e){
@@ -30,3 +46,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+
